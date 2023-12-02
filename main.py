5f19cf974b35294ee9f452671436171ebc54fa6d
qlabsphofficial <qlabsph@gmail.com>
2023-12-02 20:15:38 +0800

    g

--- main.py
+++ main.py
@@ -4,11 +4,54 @@ from sqlalchemy.orm import Session
 from database import engine, SessionLocal
 from pydantic import Field, BaseModel
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 import models
 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    student_id: str
+    password: str
+    first_name: str
+    last_name: str
+    middle_initial: str
+    status: str
+    department: str
+    course: str
+
+class UserID(BaseModel):
+    id: int
+
+
+class NoteID(BaseModel):
+    id: int
+
+class LoginData(BaseModel):
+    student_id: str
+    password: str
+
+class Note(BaseModel):
+    note_description: str
+    note_owner: int
+
+
+class Task(BaseModel):
+    task_type: str
+    task_description: str
+    due_date: str
+    task_owner: int
+
 
 
 def get_db():
@@ -18,7 +61,168 @@ def get_db():
     finally:
         db.close()
 
+@app.post('/register')
+async def register(user: User, db: Session = Depends(get_db)):
+    new_user = models.User()
 
-@app.get('/')
-async def hello():
-    return {'test': 'test works'}
+    try:
+        new_user.student_id = user.student_id
+        new_user.password = user.password
+        new_user.first_name = user.first_name
+        new_user.last_name = user.last_name
+        new_user.middle_initial = user.middle_initial
+        new_user.status = user.status
+        new_user.department = user.department
+        new_user.course = user.course
+
+        db.add(new_user)
+        db.commit()
+
+    finally:
+        db.close()
+
+    return {'response': 'success'}
+
+
+@app.post('/login')
+async def login(student: LoginData, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.student_id == student.student_id).first()
+    print(user)
+
+    if user.password == student.password:
+        return {'response': 'login success', 'user_id': user.id}
+    else:
+        return {'response': 'login failed'}
+
+
+@app.post('/get_user_data')
+async def get_user_data(user_id: UserID, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id.id).first()
+
+    return {'user_data': user, 'response': 'retrieval success'}
+
+
+@app.get('/get_notes')
+async def get_notes(id: str, db: Session = Depends(get_db)):
+    try:
+        all_notes = db.query(models.Note).filter(models.Note.note_owner == id).all()
+
+        return {'response': 'retrieval complete.', 'notes': all_notes}
+    except:
+        print('Retrieval failed.')
+        return {'response': 'retrieval failed.', 'notes': all_notes}
+
+
+@app.post('/create_note')
+async def create_note(note: Note, db: Session = Depends(get_db)):
+    try:
+        new_note = models.Note()
+
+        new_note.note_description = note.note_description
+        new_note.note_owner = int(note.note_owner)
+
+        db.add(new_note)
+        db.commit()
+
+        return {'response': 'note created.'}
+    except:
+        return {'response': 'failed to create note.'}
+    
+
+@app.post('/delete_note')
+async def delete_note(note_id: NoteID, db: Session = Depends(get_db)):
+    try:
+        retrieved_note = db.query(models.Note).filter(models.Note.id == note_id.id).first()
+        
+        if retrieved_note:
+            db.delete(retrieved_note)
+            db.commit()
+
+        return {'response': 'note deleted.'}
+    except:
+        return {'response': 'failed to delete note.'}
+    
+
+@app.get('/get_tasks')
+async def get_tasks(id: str, db: Session = Depends(get_db)):
+    try:
+        all_tasks = db.query(models.Task).filter(models.Task.task_owner == id)
+
+        all_activities = []
+        all_exams = []
+
+        for task in all_tasks:
+            task.due_date = task.due_date.strftime("%Y-%m-%d %H:%M")
+
+            if task.task_type == 'Activities':
+                all_activities.append(task)
+            else:
+                all_exams.append(task)
+
+        print(f'Printing activities: {all_activities}')
+        print(f'Printing exams: {all_exams}')
+
+        return {'response': 'tasks retrieved', 'activities': all_activities, 'exams': all_exams}
+    except:
+        return {'response': 'failed to retrieve tasks.'}
+    
+
+@app.get('/get_task_overview')
+async def get_task_overview(id: str, db: Session = Depends(get_db)):
+    # try:
+        all_tasks = db.query(models.Task).filter(models.Task.task_owner == id)
+
+        all_activities = []
+        all_exams = []
+        weekly_tasks = []
+        completed_tasks = 0
+        pending_tasks = 0
+
+        for task in all_tasks:
+            task.due_date = task.due_date.strftime("%Y-%m-%d %H:%M")
+
+            if task.task_type == 'Activities':
+                all_activities.append(task)
+            else:
+                all_exams.append(task)
+
+            # Handling Pending and Completed Tasks
+            if task.is_completed:
+                completed_tasks += 1
+            else:
+                pending_tasks += 1
+
+        return {
+            'response': 'tasks retrieved', 
+            'activities': all_activities,
+            'exams': all_exams,
+            'weekly_tasks': weekly_tasks,
+            'completed': completed_tasks,
+            'pending': pending_tasks
+        }
+    # except:
+    #     return {'response': 'failed to retrieve tasks.'}
+
+
+@app.post('/create_task')
+async def create_task(task: Task, db: Session = Depends(get_db)):
+    new_task = models.Task()
+
+    to_datetime = task.due_date[:25]
+    date_format = "%m-%d-%Y %H:%M"
+
+    new_task.task_type = task.task_type
+    new_task.task_description = task.task_description
+    new_task.due_date = datetime.strptime(to_datetime, date_format)
+    new_task.task_owner = task.task_owner
+    new_task.is_completed = False
+    
+    print(f'Task type: {new_task.task_type}')
+    print(f'Task desc: {new_task.task_description}')
+    print(f'Task date: {new_task.due_date}')
+    print(f'Task owner: {new_task.task_owner}')
+    print(f'Task status: {new_task.is_completed}')
+    db.add(new_task)
+    db.commit()
+
+    return {'response': 'task created'}
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Integer, Boolean, String, Column
+from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, Boolean, String, Column, ForeignKey, DateTime
 
 
 class User(Base):
@@ -15,3 +16,29 @@ class User(Base):
     status = Column(String)
     department = Column(String)
     course = Column(String)
+    birthday = Column(String)
+
+    notes = relationship('Note', back_populates='owner')
+    tasks = relationship('Task', back_populates='task_setter')
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    note_description = Column(String)
+    note_owner = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="notes")
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    task_type = Column(String)
+    task_description = Column(String)
+    due_date = Column(DateTime)
+    is_completed = Column(Boolean)
+    task_owner = Column(Integer, ForeignKey("users.id"))
+
+    task_setter = relationship("User", back_populates="tasks")
